6389ad98e3fab9aba9defb38e4047af42f706d07
Ludvig Lindholm <dev@llindholm.com>
2022-08-05 15:39:51 +0200

    Made django import secret key

--- dataViz/settings.py
+++ dataViz/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.utils import timezone
 
-from secret.secret import POSTGRES__PASS
+from secret.secret import POSTGRES__PASS, DJANGO_SECRET_KEY
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mk&!v3@y9dasdww&r@u%z7bf#4p@qg2&b2dqp6@8!ck3)^_@gm_v^%'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.155:8000', "localhost"]
 
 
 # Application definition
