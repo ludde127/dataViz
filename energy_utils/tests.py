42ebb48bff04287ddab740ba32b728dd77b93959
Ludvig Lindholm <ludviglindholm127@gmail.com>
2022-09-12 22:16:28 +0200

    Hopefully finally put test in right place.

--- energy_utils/tests.py
+++ energy_utils/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from energy_utils.models import TeslaTokens
+
+
+class EnergyUtils(TestCase):
+    def test_refreshes(self):
+        for t in TeslaTokens.objects.all():
+            t.refresh_tokens()
+
+    def test_is_charging(self):
+        charging_num = 0
+        for t in TeslaTokens.objects.all():
+            if t.is_charging():
+                charging_num += 1
+        print(f"Number charging is {charging_num}")
