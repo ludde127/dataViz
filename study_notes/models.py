a850c3ef7b26fb75e7966e5d70ceb1efccf60a4c
Ludvig Lindholm <ludviglindholm127@gmail.com>
2022-12-20 20:34:24 +0100

    Added code blocks and made pages look at least a bit better.

--- study_notes/models.py
+++ study_notes/models.py
@@ -1,9 +1,14 @@
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from django import forms
+from wagtail.blocks import RichTextBlock, CharBlock
+from wagtail.images.blocks import ImageChooserBlock
+from wagtailcodeblock.blocks import CodeBlock
 
 from wagtail.admin.panels import FieldPanel, InlinePanel, MultiFieldPanel
 from django.db import models
 from wagtail.search import index
+
+import blocks
 from dataViz.settings import BASE_CONTEXT
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from modelcluster.contrib.taggit import ClusterTaggableManager
@@ -41,9 +46,16 @@ class NotesIndexPage(Page):
 class NotesPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
-    body = RichTextField(blank=True)
+    #body = RichTextField(blank=True)
     tags = ClusterTaggableManager(through=NotePageTag, blank=True)
     categories = ParentalManyToManyField('study_notes.NoteCategory', blank=True)
+
+    body = StreamField([
+        ('heading', CharBlock(form_classname="title")),
+        ('paragraph', RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('code', CodeBlock(label="Code"))
+    ], use_json_field=True)
 
 
     search_fields = Page.search_fields + [
