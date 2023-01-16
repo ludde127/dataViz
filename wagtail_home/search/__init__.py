d59a5bc2cdd0bd0ac6b89613c30c6e7cbd96624b
Ludvig Lindholm <ludviglindholm127@gmail.com>
2023-01-16 18:33:05 +0100

    Flashcards kind of works.

--- wagtail_home/search/__init__.py
+++ wagtail_home/search/__init__.py
@@ -1,1 +1,6 @@
 #https://docs.wagtail.org/en/stable/topics/search/searching.html#an-example-page-search-view
+from wagtail.search.backends import get_search_backend
+from study_notes.models import NotePage
+
+s = get_search_backend()
+s.search("datorkom", NotePage)
