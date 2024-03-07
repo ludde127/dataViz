c35722039e7d4950c86b815e1fd596ba6f4917fb
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-03-07 15:43:47 +0100

    Created deploy scripts

---
+++ scripts/deploy/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the branch you want to check
+BRANCH="master"
+BASE_DIR="~/dataViz"
+cd $BASE_DIR || exit
+# Fetch the latest changes from the remote repository
+git checkout $BRANCH
+git fetch
+
+# Check if the local branch is behind the remote branch
+if [ "$(git rev-list HEAD..origin/${BRANCH} --count)" -gt 0 ] || [ "$1" = "force" ]; then
+  echo "There are new commits on the ${BRANCH} branch."
+  git stash
+  git pull -X theirs || exit
+  sh $BASE_DIR/scripts/deploy/deploy_django.sh
+  echo "Deployed"
+else
+  echo "The ${BRANCH} branch is up to date."
+fi
---
+++ scripts/deploy/deploy_django.sh
@@ -0,0 +1,4 @@
+venv/bin/pip install -r ./requirements.txt
+venv/bin/python ./manage.py makemigrations && venv/bin/python ./manage.py migrate
+venv/bin/python ./manage.py collectstatic --no-input
+systemctl restart dataViz.service
