f70ff0043b79ca8c1c20d51b85bc027bab7f381b
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-03-09 00:59:20 +0100

    themes will now update across different tabs

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,18 +1,24 @@
 const setTheme = (theme) => {
-  const root = document.querySelector(":root");
-  root.dataset.theme = theme;
-  root.style.colorScheme = theme;
-  localStorage.setItem("theme", theme);
-}
+    const root = document.querySelector(":root");
+    root.dataset.theme = theme;
+    root.style.colorScheme = theme;
+    localStorage.setItem("theme", theme);
+};
 
-if (localStorage.theme) {
-  setTheme(localStorage.theme);
-} else if ((!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  setTheme("dark");
-} else {
-  setTheme("light");
-}
+const checkTheme = () => {
+    if (localStorage.theme) {
+        setTheme(localStorage.theme);
+    } else if ((!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        setTheme("dark");
+    } else {
+        setTheme("light");
+    }
+};
 
 for (const element of document.getElementsByName("theme-dropdown")) {
-  element.addEventListener("click", _ => setTheme(element.dataset.setTheme));
+    element.addEventListener("click", _ => setTheme(element.dataset.setTheme));
 }
+
+window.addEventListener("storage", function (e) {
+    checkTheme();
+}, false);
