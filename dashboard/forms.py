730e69363c4e4d7a15b316d46b00b1b065cef5e0
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-03-10 03:28:59 +0100

    fixed some forms

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,4 +1,6 @@
+from django import forms
 from django.forms import ModelForm
+
 from dashboard.models import PlottingSetup
 
 
@@ -7,3 +9,14 @@ class PlottingSetupForm(ModelForm):
         model = PlottingSetup
         fields = ["name", "plot_type", "columns_to_plot", "index_column", "index_is_time",
                   "round_index", "x_tick_size", "y_tick_size"]
+
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "plot_type": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "columns_to_plot": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "index_column": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "index_is_time": forms.CheckboxInput({"class": "toggle"}),
+            "round_index": forms.CheckboxInput({"class": "toggle"}),
+            "x_tick_size": forms.NumberInput(attrs={"class": "input input-bordered"}),
+            "y_tick_size": forms.NumberInput(attrs={"class": "input input-bordered"}),
+        }
