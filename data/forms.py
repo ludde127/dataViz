38808da61ba4021a0472e3ae40471c6be24bb5da
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-03-14 23:48:53 +0100

    forms wip

--- data/forms.py
+++ data/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from data.models import DataStorage
+from django import forms
 
 
 class DataStorageForm(ModelForm):
@@ -7,3 +8,11 @@ class DataStorageForm(ModelForm):
         model = DataStorage
         fields = ["name", "description", "csv_names",
                   "index_column", "index_column_values_are_time"]
+
+        widgets = {
+            "name": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "description": forms.TextInput(attrs={"class": "textarea textarea-bordered"}),
+            "csv_names": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "index_column": forms.TextInput(attrs={"class": "input input-bordered"}),
+            "index_column_values_are_time": forms.CheckboxInput({"class": "toggle"}),
+        }
