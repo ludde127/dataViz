5c24bbaba2a5e17887fdd42fdb837926bd037d81
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-03-20 17:08:57 +0100

    Only render published pages in tree view
    Signed-off-by: Hussein Taher <6496177+Husenap@users.noreply.github.com>

--- study_notes/templatetags/study_notes_custom_filters.py
+++ study_notes/templatetags/study_notes_custom_filters.py
@@ -1,6 +1,8 @@
 from django import template
 
 register = template.Library()
+
+
 @register.filter
 def get_item(dictionary, key):
     """print("---")
@@ -8,3 +10,8 @@ def get_item(dictionary, key):
     print(key)
     print("...")"""
     return dictionary.get(key)
+
+
+@register.filter
+def get_live_children(page):
+    return [c for c in page.get_children() if c.live]
