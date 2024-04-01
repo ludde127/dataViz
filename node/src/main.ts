be7c73b5b21a371a567ad907e1bc1d53ffb132e3
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-04-02 00:18:12 +0200

    added new toaster
    Signed-off-by: Hussein Taher <6496177+Husenap@users.noreply.github.com>

--- node/src/main.ts
+++ node/src/main.ts
@@ -1,4 +1,5 @@
 import "./custom-elements/flashcards/subscribe-button";
 import "./custom-elements/flashcards/flashcards";
 import "./custom-elements/search/search";
+import "./custom-elements/toasts/toaster";
 import "./globals.css";
