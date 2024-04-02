09c9e76ec648e1efeab1f714d206308e564678fa
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2024-04-03 00:53:08 +0200

    updated settings and deploy script
    Signed-off-by: Hussein Taher <6496177+Husenap@users.noreply.github.com>

--- tools/deploy/deploy_django.sh
+++ tools/deploy/deploy_django.sh
@@ -5,5 +5,6 @@ grep -q '^GIT_HASH\s*=.*' .yapenv.production \
 
 venv/bin/pip install -r ./requirements.txt
 venv/bin/python ./manage.py makemigrations && venv/bin/python ./manage.py migrate
+venv/bin/python ./manage.py update_index
 venv/bin/python ./manage.py collectstatic --no-input
 systemctl restart yapity.service
